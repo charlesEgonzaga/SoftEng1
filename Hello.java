7ee107ad8c104cfc8b4b9e9e8c9f6ebe40ee8068
charlesEgonzaga <142955651+charlesEgonzaga@users.noreply.github.com>
2023-08-23 12:50:29 +0800

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+public static void main(string[] args){
+  System.out.print("Hello, Software Engineering Course!");
+  System.out.print("My Name is GONZAGA, Charles Erwin G");
+}
+}
