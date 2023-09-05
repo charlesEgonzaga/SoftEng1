1e0211b10027e9c40340f144b2476a8630fed53c
charlesEgonzaga <142955651+charlesEgonzaga@users.noreply.github.com>
2023-09-05 08:16:07 +0800

    Hello.java
    adding some changes to Hello.Java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 public static void main(string[] args){
   System.out.print("Hello, Software Engineering Course!");
   System.out.print("My Name is GONZAGA, Charles Erwin G");
+ System.out.println ("Complicated world of SoftDev that is why it pays much!");
 }
 }
